bb7fedcde6356c2415d827d533e75d3734f871cf
Bouzouita Nour El Houda <nourelhouda.bouzouita@gmail.com>
2024-07-24 12:55:30 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,7 @@
 import datetime
 
 def main():
-  print("Hello world"
-
+  print("Hello world")
   # Get the current time and print it 
   current_time = datetime.datetime.now()
   print("Current time:", current_time)
